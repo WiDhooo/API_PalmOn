e734ef8e1bf87b765806ced666875686764fa454
Ridho Putra Pratama <ridhoputra714@gmail.com>
2024-11-07 07:52:37 +0700

    api_update

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\KalkulasiController;
+use App\Http\Controllers\ArtikelController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,3 +10,4 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::apiResource('kalkulasi', KalkulasiController::class);
+Route::apiResource('artikel', ArtikelController::class);
