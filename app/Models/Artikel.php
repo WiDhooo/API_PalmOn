839b54a2052e07653056b8bbd6108321e24d1056
Ridho Putra Pratama <ridhoputra714@gmail.com>
2024-11-07 07:08:24 +0700

    update artikel

--- app/Models/Artikel.php
+++ app/Models/Artikel.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Artikel extends Model
 {
     // Tentukan nama tabel jika berbeda dari nama model yang diharapkan
-    protected $table = 'kalkulasis';
+    protected $table = 'Artikel';
 
     // Tentukan atribut yang dapat diisi secara massal
     protected $fillable = [
