3cc099d57c992bc1929507b339cdf71abb5e95eb
Ridho Putra Pratama <ridhoputra714@gmail.com>
2024-11-11 02:32:32 +0700

    update_api

--- app/Models/Kalkulasi.php
+++ app/Models/Kalkulasi.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Kalkulasi extends Model
 {
+    use HasFactory;
 
     // Tentukan nama tabel jika berbeda dari nama model yang diharapkan
     protected $table = 'kalkulasis';
